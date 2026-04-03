0d5e106a2bd20af6d68bcb3ef282a9b24707dbcb
natasza <natazaputri22@gmail.com>
2026-04-03 22:03:48 +0700

    first commit

---
+++ src/hello/world/pkg202557201034/HelloWorld202557201034.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world.pkg202557201034;
+
+/**
+ *
+ * @author Alley
+ */
+public class HelloWorld202557201034 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
